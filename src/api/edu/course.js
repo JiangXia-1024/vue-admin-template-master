23c5078bf4b5d7d5427554cc5c23dfaf19f7c8ff
jiangxia <j17621607346@163.com>
2022-03-02 22:10:14 +0800

    课程相关前端

---
+++ src/api/edu/course.js
@@ -0,0 +1,18 @@
+import request from '@/utils/request'
+export default {
+    //1 添加课程信息
+    addCourseInfo(courseInfo) {
+        return request({
+            url: '/eduservice/course/addCourseInfo',
+            method: 'post',
+            data:courseInfo
+          })
+    },
+    //2 查询所有教师
+    getListTeacher() {
+        return request({
+            url: '/eduservice/teacher/findAll',
+            method: 'get'
+          })
+    }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -87,7 +87,48 @@ export const constantRouterMap = [
       }
     ]
   },
-
+  {
+    path: '/course',
+    component: Layout,
+    redirect: '/course/list',
+    name: '课程管理',
+    meta: { title: '课程管理', icon: 'example' },
+    children: [
+      {
+        path: 'list',
+        name: '课程列表',
+        component: () => import('@/views/edu/course/list'),
+        meta: { title: '课程列表', icon: 'table' }
+      },
+      {
+        path: 'info',
+        name: '添加课程',  
+        component: () => import('@/views/edu/course/info'),
+        meta: { title: '添加课程', icon: 'tree' }
+      },
+      {
+        path: 'info/:id',
+        name: 'EduCourseInfoEdit',
+        component: () => import('@/views/edu/course/info'),
+        meta: { title: '编辑课程基本信息', noCache: true },
+        hidden: true
+      },
+      {
+        path: 'chapter/:id',
+        name: 'EduCourseChapterEdit',
+        component: () => import('@/views/edu/course/chapter'),
+        meta: { title: '编辑课程大纲', noCache: true },
+        hidden: true
+      },
+      {
+        path: 'publish/:id',
+        name: 'EduCoursePublishEdit',
+        component: () => import('@/views/edu/course/publish'),
+        meta: { title: '发布课程', noCache: true },
+        hidden: true
+      }
+    ]
+  },
 
   {
     path: '/example',
